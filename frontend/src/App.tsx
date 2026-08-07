1c6b1be8cce733b14256475afa21c7fdb5b36ba6
Heven Hailu <hibbenhailu@gmail.com>
2026-08-07 23:22:45 +0300

    feat(frontend): add TypeScript interfaces and API client service

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,40 @@
 // src/App.tsx
+import { useState } from "react";
+import "./App.css";
+import { searchRecipes } from "./api";
+import type { Recipe } from "./types";
 const App = () => {
-  return <div>Hello from Recipe App</div>;
+   const [searchTerm, setSearchTerm] = useState("burgers");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+const handleSearchSubmit = async () => {
+    try {
+      const data = await searchRecipes(searchTerm, 1);
+      setRecipes(data);
+    } catch (error) {
+      console.error("Error searching recipes:", error);
+    }
+  };
+
+  return <div>
+     <form onSubmit={handleSearchSubmit}>
+        <input
+          type="text"
+          required
+          placeholder="Enter a search term"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {recipes.map((recipe) => (
+        <div key={recipe.id}>
+          Recipe Image Location: {recipe.image}
+          <br />
+          Recipe Title: {recipe.title}
+        </div>
+      ))}
+  </div>;
 };
 
 export default App;
