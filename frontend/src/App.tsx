da828e0d1816f57e9a8034948e79d09c0315afeb
Heven Hailu <hibbenhailu@gmail.com>
2026-08-09 10:07:44 +0300

    Add components folder and update App.tsx imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,12 @@ import "./App.css";
 import { searchRecipes } from "./api";
 import type { Recipe } from "./types";
 import {  type FormEvent } from 'react';
+import RecipeCard from "./components/RecipeCard";
+
 const App = () => {
    const [searchTerm, setSearchTerm] = useState("burgers");
   const [recipes, setRecipes] = useState<Recipe[]>([]);
+  const pageNumber = useRef(1);
 
 const handleSearchSubmit = async (event: FormEvent) => {
   event.preventDefault();
@@ -17,6 +20,18 @@ const handleSearchSubmit = async (event: FormEvent) => {
       console.error("Error searching recipes:", error);
     }
   };
+  const handleViewMoreClick = async ()=>{
+    const nextPage = pageNumber.current + 1;
+  
+     try{
+         const nextRecipes = await AudioParam.searchRecipes(searchTerm,nextPage);
+         setRecipes([...recipes,...nextrecipes.results])
+         pageNumber.current = nextPage;
+     }catch(error){
+      console.log(error);
+
+     }
+  }
 
   return <div>
      <form onSubmit={(event) => handleSearchSubmit(event)}>
@@ -30,12 +45,9 @@ const handleSearchSubmit = async (event: FormEvent) => {
         <button type="submit">Submit</button>
       </form>
       {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          Recipe Image Location: {recipe.image}
-          <br />
-          Recipe Title: {recipe.title}
-        </div>
+       <RecipeCard recipe={recipe}/>
       ))}
+      <button className="view-more-button" onClick={handleViewMoreClick}>View More</button>
   </div>;
 };
 
