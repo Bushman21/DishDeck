ac993932852e41e6f1f71bcbe461c95bf313b127
Heven Hailu <hibbenhailu@gmail.com>
2026-08-13 15:03:11 +0300

    Add RecipeModal component for recipe details view

---
+++ frontend/src/components/RecipeModal.tsx
@@ -0,0 +1,14 @@
+const RecipeModal = () => {
+    return (
+        <div className="overlay">
+            <div className="modal">
+                <div className="modal-header">
+                    <h2>Recipe Details</h2>
+                </div>
+                <div className="modal-body">
+                    <p>This is where the recipe details would be displayed.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
