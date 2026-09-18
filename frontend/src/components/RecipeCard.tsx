9ee003e98892c4f5fd861466bb9ef9f592927c55
Heven Hailu <hibbenhailu@gmail.com>
2026-09-18 20:17:02 +0300

    Update RecipeCard component layout and interaction logic

--- frontend/src/components/RecipeCard.tsx
+++ frontend/src/components/RecipeCard.tsx
@@ -4,14 +4,21 @@ import { AiOutlineHeart } from "react-icons/ai";
 interface Props {
   recipe: Recipe;
   onClick: () => void;
+  onFavouriteButton: (recipe: Recipe) => void;
 }
 
-const RecipeCard = ({ recipe, onClick }: Props) => {
+const RecipeCard = ({ recipe, onClick, onFavouriteButton }: Props) => {
   return (
     <div className="recipe-card" onClick={onClick}>
       <img src={recipe.image} alt={recipe.title} />
       <div className="recipe-card-title">
-        <h3>{recipe.title}</h3>
+        <span onClick={(event) => {
+          event.stopPropagation()
+          onFavouriteButton(recipe)
+        }}>
+          <AiOutlineHeart size={25} />
+        </span>
+        <h3>{recipe.title}</h3>      
       </div>
     </div>
   );
